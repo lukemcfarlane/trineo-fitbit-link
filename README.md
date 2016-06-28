2cc5381d9db9e4297191a9c49e92cc7110cb99b9
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2016-06-28 19:57:40 +1200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+Trineo Fitbit Link
+==============================
+
+Adds link to Trineo group to header on fitbit.com.
+
+Installation
+------------
+
+1. Navigate to chrome://extensions in your Chrome browser
+2. Check "Developer Mode" on top right-hand corner of Extensions page
+3. Click "Load unpacked extension..." and select the directory that contains the contents of this repository
---
+++ contentScript.js
@@ -0,0 +1,18 @@
+const LINK = '/group/22ZMCS'
+const PHOTO_URL = chrome.extension.getURL('trineo-logo.png')
+
+$(document).ready(() => {
+  let $profileLink = $('a.nav-item-profile')
+  let $groupLink = $profileLink.clone()
+    .removeClass('nav-item-profile')
+    .addClass('nav-item-group')
+    .addClass('hidden')
+    .attr('href', LINK)
+    .attr('title', 'View Trineo group')
+  let photo = $groupLink.find('img.photo')
+    .attr('src', PHOTO_URL)
+    .load(() => {
+      $groupLink.removeClass('hidden')
+    })
+  $profileLink.after($groupLink)
+})
